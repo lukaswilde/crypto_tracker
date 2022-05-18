05be2c77e8ab2b8047c3f30784bd88b0a78c0681
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2022-05-18 19:19:49 +0200

    add mypy config, annotate types

--- prices/views.py
+++ prices/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+
 from .tasks import get_current_token_price
-import json
+
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     cardano_price = get_current_token_price("cardano")
 
     return HttpResponse(round(cardano_price["cardano"]["eur"], 2))
