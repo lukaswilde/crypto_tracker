7231b7062b0cd6c0924023e86afc07846ce0c1bc
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2022-06-20 10:45:13 +0200

    Add logic and routes for changing passwords.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,45 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.utils.translation import gettext_lazy as _
 
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
         max_length=254,
-        label="Username",
+        label=_("Username"),
         widget=forms.TextInput(attrs={"class": "form-control"}),
     )
     password = forms.CharField(
-        label="Password",
+        label=_("Password"),
         widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
+
+
+class BootstrapPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label=_("Old password"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "current-password",
+                "autofocus": True,
+                "class": "form-control",
+            }
+        ),
+    )
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "new-password", "class": "form-control"}
+        ),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "new-password", "class": "form-control"}
+        ),
     )
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
-from .forms import BootstrapAuthenticationForm
+from .forms import BootstrapAuthenticationForm, BootstrapPasswordChangeForm
 
 app_name = "accounts"
 urlpatterns = [
@@ -18,5 +18,21 @@ urlpatterns = [
         "logout/",
         auth_views.LogoutView.as_view(next_page="pages:index"),
         name="logout",
+    ),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="accounts/password_change.html",
+            form_class=BootstrapPasswordChangeForm,
+            success_url=reverse_lazy("accounts:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="accounts/password_change_done.html"
+        ),
+        name="password_change_done",
     ),
 ]
